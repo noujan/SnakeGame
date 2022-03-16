707ec9d781419b48a1535116a66209a05435d30b
Noujan Fakhri <nfakhri@haystack.ai>
2022-03-16 08:47:06 -0400

    Crashlytics

--- SnakeG/SnakeGApp.swift
+++ SnakeG/SnakeGApp.swift
@@ -23,6 +23,9 @@ struct SnakeGApp: App {
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
+        //MARK: Test code to crash the app and make sure Crashlytics is working properly
+//        let x: Int? = nil
+//        let y = x!
         return true
     }
 }
